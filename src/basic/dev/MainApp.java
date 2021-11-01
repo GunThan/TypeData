ff6fe3a8cc7a8d072e206901f672ad0d8ef788a3
GunThan <Minhdentranbinh@gamil.com>
2021-11-01 16:18:36 +0700

    first commit

---
+++ src/basic/dev/MainApp.java
@@ -0,0 +1,47 @@
+package basic.dev;
+
+public class MainApp {
+	public static void main (String[] args) {
+		//Thông tin sinh viên
+		String FullName = "Trần Hữu Bình Minh";
+		String ID = "2160168";
+		String CMND = "192002043";
+		String Class = "21CDTh41";
+		String School = "Hueic";
+		float Dtb = 9f;
+		
+		System.out.println("Thông Tin Sinh Viên");
+		System.out.println("Tên:" + FullName);
+		System.out.println("Mã Sinh Viên:" + ID);
+		System.out.println("Chứng Minh Nhân Dân:" + CMND);
+		System.out.println("Tên Lớp:" + Class);
+		System.out.println("Tên Trường:" + School);
+		System.out.println("Điểm Trung Bình:" + Dtb);
+		
+		
+		//Thông tin công nhân
+		String Name = "Trần Hữu Bình Minh";
+		String ĐC = "Quảng An, Quảng Điền, Thừa Thiên Huế";
+		String SĐT  = "0373357936";
+		String Ltb = "22.0000";
+		
+		System.out.println(" Thông Tin Công Nhân");
+		System.out.println("Tên" + Name);
+		System.out.println("Địa Chỉ:" + ĐC);
+		System.out.println("Số Điện Thoại:" + SĐT);
+		System.out.println("Lương Trung Bình:" + Ltb);
+		
+		//Thông Tin một chiếc xe car
+		String NameCar = "Mec";
+		String Hang = "Yamaha";
+		String Gia = " 22222222";
+		String Color = ("Đen");
+		
+		System.out.println(" Thông Tin Chiếc Xe");
+		System.out.println("Tên Xe:" + NameCar);
+		System.out.println("Hãng:" + Hang);
+		System.out.println("Màu Xe:" + Color);
+		
+				
+	}
+}
